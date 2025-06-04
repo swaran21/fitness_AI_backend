1f5e2d78ea619d4bc7b530b0bb398b4e9bbffeea
swaran21 <saiswaran212005@gmail.com>
2025-06-05 00:12:48 +0530

    Initial commit after activity service,user service and registering it with eureka perfectly

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,66 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    private final UserValidationService userValidationService;
+
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        boolean isValidUser = userValidationService.validateUser(request.getUserId());
+        if(!isValidUser) {
+            throw new RuntimeException("Invalid user"+request.getUserId());
+        }
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse activityResponse = new ActivityResponse();
+        activityResponse.setId(activity.getId());
+        activityResponse.setUserId(activity.getUserId());
+        activityResponse.setType(activity.getType());
+        activityResponse.setDuration(activity.getDuration());
+        activityResponse.setCaloriesBurned(activity.getCaloriesBurned());
+        activityResponse.setStartTime(activity.getStartTime());
+        activityResponse.setAdditionalMetrics(activity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(activity.getCreatedAt());
+        activityResponse.setUpdatedAt(activity.getUpdatedAt());
+        return activityResponse;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+
+        List<Activity> activities = activityRepository.findByUserId(userId);
+
+        return activities.stream().map(this::mapToResponse).toList();
+
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("Activity not found"));
+    }
+}
---
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,61 @@
+package com.fitness.userservice.service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if(repository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already exist");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        User savedUser = repository.save(user);
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreated(savedUser.getCreated());
+        userResponse.setModified(savedUser.getModified());
+
+        return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user = repository.findById(userId)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreated(user.getCreated());
+        userResponse.setModified(user.getModified());
+        return userResponse;
+    }
+
+    public Boolean existByUserId(String userId) {
+        log.info("Calling User Validation API for userId: {}",userId);
+        return repository.existsById(userId);
+    }
+}
