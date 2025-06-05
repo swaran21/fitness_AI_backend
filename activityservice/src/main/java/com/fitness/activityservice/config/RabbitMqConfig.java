27f86b99073d121837692b5d9231f31187a034e8
swaran21 <saiswaran212005@gmail.com>
2025-06-06 02:58:13 +0530

    corrected RabbitMqConfig and made business code logic

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -1,7 +1,11 @@
 package com.fitness.activityservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
@@ -10,8 +14,28 @@ import org.springframework.amqp.core.Queue;
 @Configuration
 public class RabbitMqConfig {
 
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
     public Queue activityQueue(){
-        return new Queue("activity.queue",true);
+        return new Queue(queue,true);
+    }
+
+    @Bean
+    public DirectExchange activityExchange(){
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange){
+       return BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
     }
 
     @Bean
--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,16 +5,27 @@ import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
 
     public ActivityResponse trackActivity(ActivityRequest request) {
@@ -32,6 +43,15 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        //Publishing to RabbitMQ for AI to process
+
+        try{
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        }
+        catch(Exception e){
+            log.error("Failed to publish activity to RabbitMQ : ",e);
+        }
 
         return mapToResponse(savedActivity);
     }
